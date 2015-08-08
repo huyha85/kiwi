2014d403ff0533e74c7ea03e43b0a43581c2005c
huyha85 <hhuy424@gmail.com>
2015-08-08 15:18:27 +0700

    [f] As a backend developer, I would like to validate token with SHA256 hash for api requests
    [https://www.pivotaltracker.com/story/show/100866446]

--- app/controllers/api/checkins_controller.rb
+++ app/controllers/api/checkins_controller.rb
@@ -1,8 +1,9 @@
 class Api::CheckinsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_filter :verify_token
 
   def create
-    resource = Checkin.toggle_checkin(checkin_email)
+    resource = Checkin.toggle_checkin(checkin_params[:email])
     if resource
       render json: { timestamp: resource.created_at.to_i }
     else
@@ -11,7 +12,20 @@ class Api::CheckinsController < ApplicationController
   end
 
   private
-  def checkin_email
-    params.require(:email)
+  def checkin_params
+    [:email, :random, :token].each_with_object(params) do |key, obj|
+      obj.require(key)
+    end
+  end
+
+  def verify_token
+    if server_token != checkin_params[:token]
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+      return
+    end
+  end
+
+  def server_token
+    Digest::SHA256.hexdigest("#{request.path}#{checkin_params[:email]}#{checkin_params[:random]}")
   end
 end
